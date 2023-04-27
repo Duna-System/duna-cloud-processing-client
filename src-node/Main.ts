4b4cd904373b95d8ebf27612da7762bfab66d4c2
Marcus Forte <marcus.forte@dunasystem.com>
2023-04-27 20:36:28 +0200

    testing stream

--- src-node/Main.ts
+++ src-node/Main.ts
@@ -1,10 +1,11 @@
 import { ProcessPointCloudClient } from "../gen-proto/pointcloud_grpc_pb"
 import * as grpc from "@grpc/grpc-js"
-import { Point, PointCloud, VoxelGridPointCloud } from "../gen-proto/pointcloud_pb"
+import { Point, PointCloud, VoxelFilterInput } from "../gen-proto/pointcloud_pb"
+import { Empty } from "google-protobuf/google/protobuf/empty_pb"
 
 const service = new ProcessPointCloudClient('localhost:10001', grpc.credentials.createInsecure())
 
-const voxelCloud:VoxelGridPointCloud = new VoxelGridPointCloud();
+const voxelCloud = new VoxelFilterInput();
 const pt = new Point();
 pt.setX(1)
 pt.setY(1)
@@ -16,8 +17,8 @@ pc.addPoints(pt)
 voxelCloud.setPointcloud(pc)
 voxelCloud.setResolution(1)
 
-
-service.voxelGrid(voxelCloud, (err, response) => 
-{
-
+const service_stream = service.cloudStream(new Empty) as any
+service_stream.on('data', (response:PointCloud) => {
+    console.log(response.getPointsList()[0].getX())
+    console.log(response.getPointsList()[1].getX())
 })
--- src-web/Main.ts
+++ src-web/Main.ts
@@ -1,11 +1,12 @@
 
+import { Stream } from "stream";
 import {ProcessPointCloudClient} from "../gen-proto/PointcloudServiceClientPb";
-import { Point, PointCloud, VoxelGridPointCloud} from "../gen-proto/pointcloud_pb";
-
+import { VoxelFilterInput, PointCloud, Point } from "../gen-proto/pointcloud_pb";
+import { Empty } from "google-protobuf/google/protobuf/empty_pb"
 const pcClient = new ProcessPointCloudClient("http://" + location.hostname + ":5000");
 
 // Test Voxel
-var voxelInputRequest = new VoxelGridPointCloud();
+var voxelInputRequest = new VoxelFilterInput();
 var pointCloud = new PointCloud();
 
 for (let i = 0; i < 1000; i++) {
@@ -21,17 +22,22 @@ console.log('Input cloud size', pointCloud.getPointsList().length)
 let start = performance.now()
 
 voxelInputRequest.setPointcloud(pointCloud)
-voxelInputRequest.setResolution(0.01)
+voxelInputRequest.setResolution(0.5)
 
 console.log('calling voxelGrid...')
 pcClient.voxelGrid(voxelInputRequest, null).then((replyCloud) => {
   console.log('RPC call took: ', performance.now() - start, ' ms.')
   console.log('Filtered size: ', replyCloud.getPointsList().length)
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 100032; i++) {
     // Print x values.
-    console.log(replyCloud.getPointsList()[i].getX()
-    )
+    // console.log(replyCloud.getPointsList()[i].getX())
   }
 
-
+  console.log('calling Stream...')
+  const stream = pcClient.cloudStream(new Empty)
+  stream.on("data", (response)=> {
+    
+    for ( let i = 0 ; i < response.getPointsList().length; ++i)
+      console.log(response.getPointsList()[i].getX())
+  })
 })
